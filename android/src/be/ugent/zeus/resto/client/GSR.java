7e58f0e5843bf8161114898cb40c09f6b4d3e6c3
Thomas Meire <blackskad@gmail.com>
2012-03-12 21:20:35 +0100

    Add new activities

---
+++ android/src/be/ugent/zeus/resto/client/GSR.java
@@ -0,0 +1,17 @@
+
+package be.ugent.zeus.resto.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author blackskad
+ */
+public class GSR extends Activity {
+
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+  }
+}
---
+++ android/src/be/ugent/zeus/resto/client/Info.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ugent.zeus.resto.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author blackskad
+ */
+public class Info extends Activity {
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    // ToDo add your GUI initialization code here        
+  }
+}
