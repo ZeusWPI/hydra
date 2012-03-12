a60fe619273d38206f04c30ee27809fb2fbbc543
Thomas Meire <blackskad@gmail.com>
2012-03-12 22:30:54 +0100

    Add activity class

---
+++ android/src/be/ugent/zeus/resto/client/data/Activity.java
@@ -0,0 +1,15 @@
+
+package be.ugent.zeus.resto.client.data;
+
+/**
+ *
+ * @author Thomas Meire
+ */
+public class Activity {
+  public String association_id;
+  public String title;
+  public String description;
+  public String location;
+  public String start;
+  public String end;
+}
