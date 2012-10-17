d47453f27e9e17a1f6e38bf6c7d38fd9c016ef45
feliciaan <feliciaan>
2012-10-17 17:04:19 +0200

    Changed some things to work with the CSS file

--- iOS/Hydra/SchamperDetailViewController.m
+++ iOS/Hydra/SchamperDetailViewController.m
@@ -32,18 +32,37 @@
     //[[self navigationController] setNavigationBarHidden:YES];
      
     // Do any additional setup after loading the view.
-    
-    NSString *html = [@"<h1>" stringByAppendingString:self.article.title];
-    html = [html stringByAppendingString:@"</h1><small>"];
+    NSLog(@"Date: %@",self.article.date.description);
+    NSString *html = [@"<head><link rel='stylesheet' type='text/css' href='schamper.css' /></head><body><h1>" stringByAppendingString:self.article.title];
+    html = [html stringByAppendingString:@"</h1><div class='authorAndDate'>"];
     //Date needs to be configured to send the date
-    // html = [html stringByAppendingString:[self formatDate:article.date]];
-    html = [html stringByAppendingString:@"Okt 12, 2012"];
-    html = [html stringByAppendingString:@"   --   "];
+    html = [html stringByAppendingString:[self formatDate:self.article.date]];
+    html = [html stringByAppendingString:@" door "];
     html = [html stringByAppendingString:self.article.author];
-    html = [html stringByAppendingString:@"</small>"];
+    html = [html stringByAppendingString:@"</div><div class='content'"];
     html = [html stringByAppendingString:self.article.body];
-    
-    [self.webView loadHTMLString: html baseURL:nil];
+    html = [html stringByAppendingString:@"</div></body>"];
+
+    NSString *path = [[NSBundle mainBundle] bundlePath];
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+
+    [self.webView loadHTMLString: html baseURL: baseURL];
 }
+
+ - (NSString *) formatDate: (NSDate *) date {
+     // returns temporary today instead of the publishing date because it has a NULL value!!!
+     // needs to be like "13 oktober 2012 om 22.57 uur"
+     NSLocale *dutch = [[NSLocale alloc] initWithLocaleIdentifier:@"nl_NL"];
+     NSString *formatString = [NSDateFormatter dateFormatFromTemplate:@"dd MMMM YYYY om hh.mm uur" options:0 locale:dutch];
+     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+     [dateFormatter setDateFormat:formatString];
+
+
+     NSString *todayString = [dateFormatter stringFromDate:[NSDate date]];
+     NSLog(@"todayString: %@", todayString);
+     return todayString;
+ }
+ 
+
 
 @end
