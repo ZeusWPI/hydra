00c3960fb0c9bc959544be8e8f9fd62f03345408
Toon Willems <willemstoon@gmail.com>
2013-01-07 11:23:11 +0100

    initial work on scrollable legend view

--- iOS/Hydra/RestoInfoView.m
+++ iOS/Hydra/RestoInfoView.m
@@ -33,51 +33,57 @@
 - (void)createView
 {
     // background
+
     UIImage *background = [UIImage imageNamed:@"header-bg.png"];
     UIImageView *backgroundView = [[UIImageView alloc] initWithFrame:self.bounds];
     backgroundView.image = background;
     backgroundView.contentMode = UIViewContentModeScaleToFill;
     backgroundView.autoresizingMask = self.autoresizingMask;
     [self addSubview:backgroundView];
+    
+    
+    // Header view
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0,0,self.frame.size.width,220)];
 
     // logo
     UIImage *logo = [UIImage imageNamed:@"resto-logo.png"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:logo];
-    [imageView setFrame:CGRectMake(90, 20, 100, 100)];
-    [self addSubview:imageView];
+    [imageView setFrame:CGRectMake(90, 10, 100, 100)];
+    [headerView addSubview:imageView];
 
     // resto info
-    UILabel *infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 133, 240, 80)];
+    UILabel *infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, imageView.frame.size.height + 10, self.frame.size.width, 80)];
     infoLabel.text = @"De resto's van de UGent zijn elke weekdag open van 11u15 tot 14u. 's Avonds kan je ook terecht in resto De Brug van 17u30 tot 21u.";
     infoLabel.backgroundColor = [UIColor clearColor];
     infoLabel.textColor = [UIColor whiteColor];
-    infoLabel.font = [UIFont systemFontOfSize:15];
+    infoLabel.font = [UIFont systemFontOfSize:14];
     infoLabel.textAlignment = UITextAlignmentCenter;
     infoLabel.numberOfLines = 4;
-    [self addSubview:infoLabel];
+    [headerView addSubview:infoLabel];
     
-    //tableview
-    CGRect tableFrame = CGRectMake(20, 220, self.frame.size.width-40, self.frame.size.height-310);
-    UITableView *tableView = [[UITableView alloc] initWithFrame:tableFrame style:UITableViewStylePlain];
-    tableView.delegate = self;
-    tableView.dataSource = self;
-    tableView.bounces = NO;
-    tableView.separatorColor = [UIColor clearColor];
-    tableView.backgroundColor = [UIColor clearColor];
-    tableView.allowsSelection = NO;
-    tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableFrame.size.width, 15)];
-    [self addSubview:tableView];
-    self.tableView = tableView;
-    
-    CGRect titleFrame = CGRectMake(0, 0, tableFrame.size.width, 20);
+    // title
+    CGRect titleFrame = CGRectMake(0, infoLabel.frame.origin.y + 80, self.frame.size.width, 20);
     UILabel *headerTitle = [[UILabel alloc] initWithFrame:titleFrame];
     headerTitle.text = @"Legende";
     headerTitle.textAlignment = NSTextAlignmentCenter;
     headerTitle.font = [UIFont boldSystemFontOfSize:18];
     headerTitle.textColor = [UIColor whiteColor];
     headerTitle.backgroundColor = [UIColor clearColor];
-    self.tableView.tableHeaderView.backgroundColor = [UIColor clearColor];
-    [self.tableView.tableHeaderView addSubview:headerTitle];
+    [headerView addSubview:headerTitle];
+    
+    // Tableview
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0,0, self.bounds.size.width, self.bounds.size.height)                                                              style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.bounces = NO;
+    tableView.separatorColor = [UIColor clearColor];
+    tableView.backgroundColor = [UIColor clearColor];
+    tableView.allowsSelection = NO;
+    tableView.tableHeaderView = headerView;
+    [self addSubview:tableView];
+    self.tableView = tableView;
+    
 }
 
 - (void)setLegend:(NSArray *)legend
@@ -99,7 +105,7 @@
     static NSString *cellIdentifier = @"RestoLegendViewCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     if(!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2
                                       reuseIdentifier:cellIdentifier];
         //set cell textLabel, contains value
         [cell.detailTextLabel setTextColor:[UIColor whiteColor]];
