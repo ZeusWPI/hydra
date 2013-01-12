b4e0429d15d9d93e0150ad9dcdc4bb574d7cb0b1
Tom Naessens <Tom.Naessens@UGent.be>
2013-01-12 22:16:54 +0100

    Increased the height on the options

--- android/src/be/ugent/zeus/hydra/ui/info/InfoList.java
+++ android/src/be/ugent/zeus/hydra/ui/info/InfoList.java
@@ -54,7 +54,9 @@ public class InfoList implements ListAdapter {
     text.setText(((NSString) dict.objectForKey("title")).toString());
     text.setGravity(Gravity.CENTER_VERTICAL);
     text.setPadding(5, 5, 5, 0);
-    text.setHeight(62);
+    
+    double density = context.getResources().getDisplayMetrics().density;
+    text.setHeight((int) (48.0 * density + 0.5));
 
     NSString image = (NSString) dict.objectForKey("image");
     if (image != null) {
