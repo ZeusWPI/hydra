87de6cbfa9ecc83394e796230df45197d5e97004
feliciaan <feliciaan@de-palmenaer.eu>
2013-09-17 22:46:27 +0200

    forgot to save the path

--- iOS/Hydra/InfoWebViewController.m
+++ iOS/Hydra/InfoWebViewController.m
@@ -25,6 +25,8 @@
     // Trigger view laod
     [self view];
 
+    self.path = path;
+
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@",kCachingBaseURL, path]];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:5.0];
     [self.webView loadRequest:request];
@@ -32,10 +34,11 @@
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
 {
-    NSLog(@"Error: WebView:%@",error.localizedDescription);
+    NSLog(@"Error WebView: %@ for path: %@",error.localizedDescription, self.path);
     NSString *path = [[NSBundle mainBundle] pathForResource:self.path ofType:nil];
     if (path) {
-        [super loadHtml:path];
+        NSURL *url = [[NSBundle mainBundle] URLForResource:self.path withExtension:nil];
+        [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
     }
 }
 @end
