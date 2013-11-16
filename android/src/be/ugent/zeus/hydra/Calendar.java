9edb52f80e7fa6760e65d979d8f5b66c28ed0bca
Stijn Seghers <seghers_stijn@hotmail.com>
2013-11-16 22:32:02 +0100

    Obsolete log import

--- android/src/be/ugent/zeus/hydra/Calendar.java
+++ android/src/be/ugent/zeus/hydra/Calendar.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
