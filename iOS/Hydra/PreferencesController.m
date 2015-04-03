513ff8664776d598043fa0f27c7dc238645ca8ad
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2015-04-03 20:20:53 +0200

    Fixes #192 and some more layout.

--- iOS/Hydra/PreferencesController.m
+++ iOS/Hydra/PreferencesController.m
@@ -104,7 +104,8 @@
                     cell.textLabel.text = @"Toon alle verenigingen";
                     cell.detailTextLabel.text = @"";
 
-                    CGRect toggleRect = CGRectMake(225, 9, 0, 0);
+                    CGFloat screen_width = self.view.frame.size.width;
+                    CGRect toggleRect = CGRectMake(screen_width - 60, 9, 0, 0);
                     UISwitch *toggle = [[UISwitch alloc] initWithFrame:toggleRect];
                     toggle.tag = kSwitchTag;
                     toggle.on = !prefs.filterAssociations;
@@ -212,7 +213,8 @@
 - (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
 {
     if (section == kFilterSection) {
-        CGRect infoRect = CGRectMake(10, 3, 300, 58);
+        CGFloat screen_width = self.view.frame.size.width;
+        CGRect infoRect = CGRectMake(10, 3, screen_width - 20, 58);
         UILabel *info = [[UILabel alloc] initWithFrame:infoRect];
         info.backgroundColor = [UIColor clearColor];
         info.font = [UIFont systemFontOfSize:14];
