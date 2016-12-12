b432f9834151bf69a30159aa4446387831813fcf
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2016-12-12 21:16:35 +0100

    Delete some stuff, it will live forever in the git history

--- iOS/Hydra/NSMutableArray+Shuffling.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-// This category enhances NSMutableArray by providing
-// methods to randomly shuffle the elements.
-@interface NSMutableArray (Shuffling)
-
-- (void)H_shuffle;
-
-@end
--- iOS/Util/sort-associations.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from plistlib import readPlist, writePlist
-from sys import stdin, stdout
-
-plist = readPlist(stdin)
-plist = sorted(plist, key = lambda k: (k['parentAssociation'], k['internalName']))
-writePlist(plist, stdout)
