ccf81a9db56e57df439b91c163e503d54e2e337f
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2017-10-17 00:43:07 +0200

    Show the current resto.sh stage

--- scraper/resto.sh
+++ scraper/resto.sh
@@ -8,8 +8,15 @@ rm -f resto/1.0/week
 ln -s "menu/$(date +%Y)" resto/1.0/week
 
 # Run scraper
+echo "Scraping all the restomenus"
 python3 resto.py
+
+echo "Eating all the sandwiches"
 python3 sandwiches.py
+
+echo "Finding all the desserts"
 python3 cafetaria.py
+
+echo "Pushing everything to the web"
 rsync -a resto/1.0/ ~/public/api/1.0/resto/
 rsync -a resto/2.0/ ~/public/api/2.0/resto/
