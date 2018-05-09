0b10156f1c83ee4a8934aebdec8f60548627995e
Niko Strijbol <strijbol.niko@gmail.com>
2018-05-09 21:32:52 +0200

    Add test scripts and travis (maybe works?)

---
+++ validation/validator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! command -v 'ajv' >/dev/null 2>&1; then
+    echo >&2 "Run 'npm install -g ajv-cli' to validate the json. Aborting."
+    exit 1
+fi
+
+validate() {
+    filename=$(basename -- "$1")
+    filename="${filename%.*}"
+    echo "Checking $1..."
+    ajv test -s "schema/${filename}" -d "$1" --valid
+    exit_status=$?
+    return ${exit_status}
+}
+
+# Add new tests here.
+
+validate '../scraper/info/info-content.json' &&
+validate '../scraper/association/special_events.json'
+
+exit $?
