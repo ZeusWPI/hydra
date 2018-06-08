ca5a05a320f06665a77c80b80fda2f8ce02c4002
Wout Schellaert <wout.schellaert@gmail.com>
2018-06-08 15:42:55 +0200

    Add script for easily applying manual changes to menu

--- scraper/resto.sh
+++ scraper/resto.sh
@@ -11,6 +11,9 @@ ln -s "menu/$(date +%Y)" resto/1.0/week
 echo "Scraping all the restomenus"
 python3 resto.py
 
+echo "Applying manual changes"
+python3 resto_manual.py
+
 echo "Eating all the sandwiches"
 python3 sandwiches.py
 
@@ -18,5 +21,5 @@ echo "Finding all the desserts"
 python3 cafetaria.py
 
 echo "Pushing everything to the web"
-rsync -a resto/1.0/ ~/public/api/1.0/resto/
-rsync -a resto/2.0/ ~/public/api/2.0/resto/
+# rsync -a resto/1.0/ ~/public/api/1.0/resto/
+# rsync -a resto/2.0/ ~/public/api/2.0/resto/
