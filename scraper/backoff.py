bbf475bc41f81aa3b2bee320a49dd47193141baf
redfast00 <redfast00@gmail.com>
2018-06-09 21:06:26 +0200

    Handle not connecting

--- scraper/backoff.py
+++ scraper/backoff.py
@@ -1,10 +1,24 @@
 import requests
-from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-retry_session = requests.Session()
-# Try 0.0s, 0.25s, 0.5s, 1s, 2s
-retries = Retry(total=5, backoff_factor=0.25)
+TIMEOUT = 5  # Time before a request times out
+BACKOFF = 0.25  # Try 0.0s, 0.25s, 0.5s, 1s, 2s between requests
+AMOUNT = 5  # Amount of request to make before giving up
 
-retry_session.mount('http://', HTTPAdapter(max_retries=retries))
-retry_session.mount('https://', HTTPAdapter(max_retries=retries))
+
+class TimeoutHTTPAdapter(requests.adapters.HTTPAdapter):
+    def __init__(self, timeout=None, *args, **kwargs):
+        self.timeout = timeout
+        super().__init__(*args, **kwargs)
+
+    def send(self, *args, **kwargs):
+        if 'timeout' not in kwargs or kwargs['timeout'] is None:
+            kwargs['timeout'] = self.timeout
+        return super().send(*args, **kwargs)
+
+
+retry_session = requests.Session()
+retries = Retry(total=AMOUNT, backoff_factor=BACKOFF)
+
+for type in ('http://', 'https://'):
+    retry_session.mount(type, TimeoutHTTPAdapter(timeout=5, max_retries=retries))
