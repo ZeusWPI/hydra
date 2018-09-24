14813b319924636988f824ec6c6dddac740ac589
Niko Strijbol <strijbol.niko@gmail.com>
2018-09-24 16:32:01 +0200

    Fix paths to schemas again again

--- server/tests/info.sh
+++ server/tests/info.sh
@@ -5,8 +5,8 @@
 
 # Execute checks.
 prefix="../src/info"
-./validators/json.sh "$prefix/nl/info-content.json" "../api/schema/info-content.json" &&
-./validators/json.sh "$prefix/en/info-content.json" "../api/schema/info-content.json" &&
+./validators/json.sh "$prefix/nl/info-content.json" "../src/schema/info-content.json" &&
+./validators/json.sh "$prefix/en/info-content.json" "../src/schema/info-content.json" &&
 ./validators/html.sh "$prefix"
 
 # Return the status code of the validator.
