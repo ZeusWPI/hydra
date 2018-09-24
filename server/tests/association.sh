fc09e34762bd538ef8bd5f16900f807edbeafc49
Niko Strijbol <strijbol.niko@gmail.com>
2018-09-24 16:20:11 +0200

    Fix paths to schemas again

--- server/tests/association.sh
+++ server/tests/association.sh
@@ -4,7 +4,7 @@
 # This script will be run from in the "tests" directory.
 
 # Check the syntax of the special events.
-./validators/json.sh "../src/association/special_events.json" "../src/schema/special_event.json"
+./validators/json.sh "../src/association/special_events.json" "../src/schema/special_events.json"
 
 # Return the status code of the validator.
 exit $?
