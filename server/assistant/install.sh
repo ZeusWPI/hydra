789335e79494c70505001eb877ac8179a9bc8651
Niko Strijbol <strijbol.niko@gmail.com>
2018-11-14 23:22:56 +0100

    Move assistant and add install script for assistant

---
+++ server/assistant/install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# First argument is input, second is output.
+set -euo pipefail
+
+if [[ $# -lt 2 ]]; then
+    echo "Missing parameters." >&2
+    echo "Use: install.sh source output" >&2
+    exit 2
+fi
+
+if [[ ! -d "$1" ]]; then
+    echo "The input directory '$1' is not a valid path." >&2
+    exit 2
+fi
+
+if ! command -v npm &> /dev/null ; then
+    echo "The assistant requires npm to be installed and callable." >&2
+    exit 2
+fi
+
+if [[ -d "$2" ]]; then
+    rm -r "$2"
+fi
+
+# Make sure we are in the correct directory.
+cd "$1"
+
+echo "Installing Google Assistant"
+echo -n "Setting up files..."
+
+# Use absolute paths
+input=$(realpath "$1")
+output=$(realpath "$2")
+
+# We don't copy the files, but symlink to this directory.
+ln -snf "$input" "$output"
+
+echo " OK"
+echo "Installing server..."
+npm install
---
+++ tests/README.md
@@ -0,0 +1,3 @@
+# Tests
+
+Contains some scripts that can be used to test things, as well as specification to test against.
