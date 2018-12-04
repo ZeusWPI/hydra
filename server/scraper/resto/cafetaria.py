0e2d058efd310c2060df2a86b1de989eca1e2ea5
Niko Strijbol <strijbol.niko@gmail.com>
2018-12-04 19:27:51 +0100

    Modify cafetaria scraper

--- server/scraper/resto/cafetaria.py
+++ server/scraper/resto/cafetaria.py
@@ -1,13 +1,13 @@
+import argparse
+import os
+
 from backoff import retry_session
 from bs4 import BeautifulSoup
-from util import parse_money, stderr_print
-import json
+from util import parse_money, write_json_to_file
 import sys
 from requests.exceptions import ConnectionError, Timeout
 
 HTML_PARSER = 'lxml'
-OUTFILE = "resto/2.0/extrafood.json"
-
 BASE_URL = 'https://www.ugent.be/student/nl/meer-dan-studeren/resto/ophetmenu/'
 
 
@@ -44,11 +44,28 @@ def get_desserts():
     return data
 
 
+def main(output):
+    result = {
+        'breakfast': get_breakfast(),
+        'drinks': get_drinks(),
+        'desserts': get_desserts()
+    }
+
+    output_file = os.path.join(output, "extrafood.json")
+    write_json_to_file(result, output_file)
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run cafetaria scraper')
+    parser.add_argument('output',
+                        help='Path of the folder in which the output must be written. Will be created if needed.')
+    args = parser.parse_args()
+
+    output_path = os.path.abspath(args.output)  # Like realpath
+    os.makedirs(output_path, exist_ok=True)  # Like mkdir -p
+
     try:
-        data = {'breakfast': get_breakfast(), 'drinks': get_drinks(), 'desserts': get_desserts()}
+        main(output_path)
     except (ConnectionError, Timeout) as e:
-        stderr_print("Failed to connect: ", e)
+        print("Failed to connect: ", e, file=sys.stderr)
         sys.exit(1)
-    with open(OUTFILE, 'w') as outfile:
-        json.dump(data, outfile, sort_keys=True, indent=4, separators=(',', ': '))
