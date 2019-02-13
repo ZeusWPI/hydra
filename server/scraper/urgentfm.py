cfaf1efc98ef50a9eea3aa35d2b17a883f6a5fb8
Niko Strijbol <strijbol.niko@gmail.com>
2019-02-13 18:34:27 +0100

    Parse Urgent.fm programme information (#293)

--- server/scraper/urgentfm.py
+++ server/scraper/urgentfm.py
@@ -20,7 +20,10 @@ LIVE_URL = 'http://urgent.fm/listen_live.config'
 def get_programme():
     response = retry_session.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
-    return soup.select('#header-text > a')[-1].text
+    link = soup.select('#header-text > a')[-1]
+    programme_name = link.text
+    programme_link = link['href']
+    return programme_name, programme_link
 
 
 def get_stream_link():
@@ -29,6 +32,15 @@ def get_stream_link():
     :return: The link.
     """
     return retry_session.get(LIVE_URL).text.strip()
+
+
+def get_programme_description(link):
+    response = retry_session.get(URL + link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = soup.select('.content')[0]
+    img = content.select('.field-name-field-radioprograms-image img')[0]['src']
+    text = content.select('.field-type-text-with-summary')[0].text
+    return img, text
 
 
 def run(output):
@@ -41,11 +53,21 @@ def run(output):
     output_file = os.path.join(output_path, 'status.json')  # Output file
 
     stream_link = get_stream_link()
-    programme = get_programme()
+    programme, programme_link = get_programme()
+    try:
+        programme_image, programme_description = get_programme_description(programme_link)
+    except IndexError:
+        # This means there is probably no information.
+        programme_image, programme_description = None, None
 
     result = {
         'url': stream_link,
         'name': programme,
+        'meta': {
+            'name': programme,
+            'image': programme_image,
+            'description': programme_description
+        },
         'validUntil': (datetime.now() + timedelta(hours=1)).isoformat()
     }
     write_json_to_file(result, output_file)
