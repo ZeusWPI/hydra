4109f824cdde128d1e397473d3d3ad04bde747a3
mcbloch <github@mcbloch.dev>
2022-11-07 22:46:27 +0100

    Add intelligent parsing of the cron schedule and success state

--- server/admin/app.py
+++ server/admin/app.py
@@ -7,6 +7,9 @@ import time
 from flask import Flask
 from flask import render_template
 
+from cron_converter import Cron
+from cron_descriptor import get_description, ExpressionDescriptor
+
 import config
 from datedifference import humanize_date_difference
 
@@ -25,17 +28,36 @@ def home():
         file_stats = os.stat(f"{app.config['PUBLIC_DIR']}/{scrape_check['last_modified_file_path']}")
 
         last_modification_time = datetime.fromtimestamp(file_stats[stat.ST_MTIME])
-        last_modification_time_pretty = humanize_date_difference(
-            now=now,
-            otherdate=last_modification_time)
         data = scrape_check
-        data['last_modification_time'] = last_modification_time_pretty
-        data['scrape_failed_for_1_day'] = (now - last_modification_time) > timedelta(days=1)
+        data['last_modification_time'] = last_modification_time
+        data['last_modification_time_pretty'] = humanize_date_difference(now=now, otherdate=last_modification_time)
         scrape_status_results.append(data)
+
+    with open(f"{app.config['SCRAPER_DIR']}/hydra.cron", "r") as cronfile:
+        for line in cronfile.readlines():
+            if line[0] == "#":
+                continue
+            cron_schedule_str = " ".join(line.split(" ")[:5])
+            cron_instance = Cron()
+            cron_instance.from_string(cron_schedule_str)
+            cron_pretty = ExpressionDescriptor(cron_schedule_str)
+
+            # Raw datetime without timezone info (not aware)
+            reference = datetime.now()
+            # Get the iterator, initialised to now
+            schedule = cron_instance.schedule(reference)
+           
+            for scraper in scrape_status_results:
+                if scraper["cron_scriptname"] in line:
+                    scraper["cron_instance"] = cron_instance
+                    scraper["cron_pretty"] = cron_pretty
+                    scraper["last_scrape_failed"] = schedule.prev() > scraper["last_modification_time"]
+                    break
+
 
     return render_template(
         'index.html',
-        scrape_status_results=scrape_status_results
+        scrape_status_results=scrape_status_results,
     )
 
 
--- server/admin/config.py
+++ server/admin/config.py
@@ -1,28 +1,31 @@
 import os
 
 PUBLIC_DIR = os.environ.get("PUBLIC_DIR")
+SCRAPER_DIR = os.environ.get("SCRAPER_DIR")
 
 if not PUBLIC_DIR:
     raise ValueError("No PUBLIC_DIR set for Flask application")
+if not SCRAPER_DIR:
+    raise ValueError("No SCRAPER_DIR set for Flask application")
 
 LAST_SCRAPED_FILE = [{
     'name': 'Resto scraper',
     'last_modified_file_path': 'api/2.0/resto/menu/nl/overview.json',
     'data_url': 'https://hydra.ugent.be/api/2.0/resto/',
-    'scrape_interval': 'every day at 10 am and 8 pm'
+    'cron_scriptname': "resto.sh",
 }, {
     'name': 'Schamper scraper',
     'last_modified_file_path': 'api/1.0/schamper/daily.json',
     'data_url': 'https://hydra.ugent.be/api/1.0/schamper/',
-    'scrape_interval': 'every day at 9 am'
+    'cron_scriptname': "schamper.py",
 }, {
     'name': 'UGent news scraper',
     'last_modified_file_path': 'api/2.0/news/nl.json',
     'data_url': 'https://hydra.ugent.be/api/2.0/news/',
-    'scrape_interval': 'every day at 8 am'
+    'cron_scriptname': "news.py",
 }, {
     'name': 'Urgent.fm scraper',
     'last_modified_file_path': 'api/2.0/urgentfm/status.json',
     'data_url': 'https://hydra.ugent.be/api/2.0/urgentfm/',
-    'scrape_interval': 'every half hour'
+    'cron_scriptname': "urgentfm.py",
 }]
